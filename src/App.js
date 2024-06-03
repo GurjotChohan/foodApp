08c66ecd8126ae7ae674c19fdee6c2d5c9f9e771
GurjotChohan <112025372+GurjotChohan@users.noreply.github.com>
2024-06-03 16:59:06 -0500

    send

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [query, setquery] = useState("")
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Food Recipe Plaza</h1>
+      <form className='app_search'>
+        <input type="text" 
+        placeholder = "enter ingreident" 
+        value = {query} 
+        onChange={(e) => setquery(e.target.value)}/>
+        <input type="sumbit" value = "Search"/>
+        </form>
     </div>
   );
 }
